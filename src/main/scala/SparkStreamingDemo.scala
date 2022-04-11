c2081bee4d12d127fc5673e18cea70630e373069
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-11 15:05:58 +0200

    update

--- src/main/scala/SparkStreamingDemo.scala
+++ src/main/scala/SparkStreamingDemo.scala
@@ -1,10 +1,8 @@
 import org.apache.spark.sql.SparkSession
 
-import scala.::
-
 object SparkStreamingDemo extends App {
   val path = "C:\\spark\\Spark-streaming\\files"
-  import org.apache.spark.sql.functions._
+
   import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
 
   val spark = SparkSession
@@ -12,24 +10,23 @@ object SparkStreamingDemo extends App {
     .master("local[*]")
     .getOrCreate()
 
-
   val mySchema = StructType(
-    StructField("id", LongType, nullable = false) ::
-    StructField("city", StringType, nullable = false) ::
-    StructField("country", StringType, nullable = false) :: Nil )
+      StructField("id", LongType, nullable = false) ::
+      StructField("city", StringType, nullable = false) ::
+      StructField("country", StringType, nullable = false) :: Nil
+  )
 
   val data = spark
     .readStream
     .format("csv")
-    .option("header",true)
+    .option("header", value = true)
     .schema(mySchema)
     .load(path)
-
 
   val output = data.writeStream
     .format("console")
     .start
-    output.awaitTermination
+  output.awaitTermination
 }
 
 //  val newFile = data
